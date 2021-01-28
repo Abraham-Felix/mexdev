f7744dee3fd8096ccc52f909e188ad57459c4f30
abraham-felix <servicioscelfonica@gmail.com>
2021-01-27 21:26:02 -0700

    mexdev color scheme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -16,3 +16,11 @@ const vuetify = new Vuetify({
 })
 
 export default vuetify
+
+
+/* mexdec color scheme
+.mexdev-1-hex { color: #313233; }
+.mexdev-2-hex { color: #5388E6; }
+.mexdev-3-hex { color: #DADCE0; }
+.mexdev-4-hex { color: #465061; }
+.mexdev-5-hex { color: #A8AAAD; } */
