3f86c86fa8370493e1a01cdcff1e93a5e835d34f
abraham-felix <apesyntax@gmail.com>
2021-08-29 22:44:13 -0700

    requests | layout optimization

--- src/views/Requests.vue
+++ src/views/Requests.vue
@@ -21,15 +21,18 @@
 }
 </style>
 <template>
-  <v-container>
+<main>
     <WorkRequest/>
     <MyRequests/>
+    <div class="mt-n15">
+    <h1> Requests </h1>
     <v-card>
-        <p style="text-align: center;">" Fill in your request form start by pressing the <span class="mdi mdi-plus"> ,</span><br>
+        <p style="text-align: center;">" Fill in your request form! you can start by pressing the <span class="mdi mdi-plus"> ,</span><br>
          you will be contacted shortly by one of our developers "</p>
          <img class="tutorial-giff" src="https://firebasestorage.googleapis.com/v0/b/mexdev-40fff.appspot.com/o/giff%2FMexdev%20Wrok%20Request.gif?alt=media&token=fa370977-d60b-48da-8667-b3704f79e753"/>
       </v-card>
-  </v-container>
+    </div>
+</main>
 </template>
 
 <script>
