84a4f651d8a7dda35558ee279e159b8d92eadb32
abraham-felix <apesyntax@gmail.com>
2021-09-05 12:38:41 -0700

    about ++

--- src/views/About.vue
+++ src/views/About.vue
@@ -71,21 +71,21 @@ h4 {
     <h1> About us </h1>
     <v-img class="center fav" src="../assets/mxdv-fav.png" />
     <v-card-text class="text-justify p10s">
-        <h4><b>About Vision:</b></h4>
+        <h4><b>Vision:</b></h4>
         <br>
         <v-divider class="mb-5"></v-divider>
-        <v-img src="../assets/home-office.png" />
+        <v-img class="mb-5" src="../assets/home-office.png" />
         <p>
-            MexDev is a web development team providing coding solutions as a service for individuals, companies & contractors, upgrading into the online business model or updating their current app progress.
+            Hello, it's <a target="_blank" href="https://www.github.com/abraham-felix">Abraham Felix</a>, I would love to introduce you to <b>MexDev</b> is a web development team providing coding solutions as a service for individuals, companies & contractors, upgrading into the online business model or updating their current app progress, we are home office individuals with great energy.
         </p>
-        <h4 class="mt-10"><b>About the project:</b></h4>
+        <h4 class="mt-10"><b>The project:</b></h4>
         <br>
         <v-divider></v-divider>
         <br>
         <p>MexDev.io Is a brand new fresh remote project with project founders in the USA and MX. we are bilingual. we got more than 15 years of combined software development experience.
            we currently have to funds but we povide all with our fingers skills programing and developing our own products :D, all you see in this side has been coded to maximize the UX. </p>
         <br>
-        <h4 class="mt-10"><b>Project Vision, Objectives and Services:</b> </h4>
+        <h4 class="mt-10"><b>Project services & objectives:</b> </h4>
         <p>
             <br>
             <br> Software development talent is expensive. Hiring new developers for a project is risky too, as a developer's skills may not match their resume and because some engineers fail to keep current on the latest languages, platforms and best practices.
