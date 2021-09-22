8d2b055d58209d00f3b8cf96ed356b2c5d096616
abraham-felix <apesyntax@gmail.com>
2021-09-21 22:16:01 -0700

    animates slide refactoring

--- src/components/AnimatedSlide.vue
+++ src/components/AnimatedSlide.vue
@@ -1,15 +1,130 @@
+<template>
+<div>
+    <v-divider  />
+
+      <h3 class="as-h3 text-body-2">Get back on track with top technologies:</h3>
+    <section id="animate-area">
+         <v-row>
+           <v-col
+           v-for="(img,i) in imgs"
+           :key="i"
+           >
+             <a
+              target="_blank"
+             :href="img.href">
+               <v-img
+               class="hover-up "
+               height="46px"
+               contain
+               :title="img.title"
+               :src="img.src">
+               </v-img>
+             </a>
+           </v-col>
+         </v-row>
+    </section>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'AnimatedSlide',
+    data() {
+          return {
+        imgs:[
+          {
+            src: require("@/assets/tech-icons/vue-logo-46px.png"),
+            title: "vue",
+            href: "https://vuejs.org/"
+          },
+          {
+            src: require("@/assets/tech-icons/vuex-logo-46px.png"),
+            title: "vuex",
+            href: "https://vuex.vuejs.org/"
+          },
+          {
+            src: require("@/assets/tech-icons/vueitfy-logo-46px.png"),
+            title: "vuetify",
+            href: "https://vuetifyjs.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/bootstrap-logo-46px.png"),
+            title: "bootstrap",
+            href: "https://getbootstrap.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/css3-logo-46px.png"),
+            title: "css3",
+            href: "https://developer.mozilla.org/es/docs/Web/CSS"
+          },
+          {
+            src: require("@/assets/tech-icons/html5-logo-46px.png"),
+            title: "html5",
+            href: "https://developer.mozilla.org/es/docs/Glossary/HTML5"
+          },
+          {
+            src: require("@/assets/tech-icons/js-logo-46px.png"),
+            title: "js",
+            href: "https://developer.mozilla.org/es/docs/Web/JavaScript"
+          },
+          {
+            src: require("@/assets/tech-icons/ror-logo-46px.png"),
+            title: "ruby on rails",
+            href: "https://rubyonrails.org/"
+          },
+          {
+            src: require("@/assets/tech-icons/sheets-logo-46px.png"),
+            title: "sheets",
+            href: "https://www.google.com/sheets/about/"
+          },
+          {
+            src: require("@/assets/tech-icons/firebase-logo-46px.png"),
+            title: "firebase",
+            href: "https://www.firebase.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/git-logo-46px.png"),
+            title: "git",
+            href: "https://www.git.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/github-logo-46px.png"),
+            title: "github",
+            href: "https://www.github.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/figma-logo-46px.png"),
+            title: "figma",
+            href: "https://www.figma.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/corel-logo-46px.png"),
+            title: "corel",
+            href: "https://www.corel.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/sg-logo-46px.png"),
+            title: "screen to gif",
+            href: "https://www.screentogif.com/"
+          },
+          {
+            src: require("@/assets/tech-icons/drawio-logo-46px.png"),
+            title: "draw.io",
+            href: "https://www.draw.io"
+          }
+        ]
+    }
+  }
+}
+
+</script>
 <style>
 
 .as-h3 {
     color: #47484a;
 }
-
-.coolh1 {
-    padding-top: 35x;
-}
-
 .newfeaturesheader {
-    height: 150px;
     font-size: 30px;
     text-align: center;
     font-family: 'Poppins', sans-serif;
@@ -61,11 +176,16 @@
     }
 }
 
+.hover-up:hover {
+  margin-top: -15px;
+  transition: 0.5s;
+}
+
 #animate-area {
     width: 100%;
     position: inherit;
-    height: 150px;
-    background-image: url(../assets/just-clouds.png);
+    height: 120px;
+    background-image: url(../assets/graphics/just-clouds.png);
     background-position: 0px 0px;
     animation: animatedBackground 10s linear infinite;
     -moz-animation: animatedBackground 10s linear infinite;
@@ -75,26 +195,3 @@
 }
 
 </style>
-
-<template>
-
-<div>
-    <h3 class="as-h3">Get back on track with top technologies:</h3>
-    <v-divider class="my-10" />
-
-          <v-img class="mx-16 mb-n16" src="../assets/UxUi-tech.png" />
-    <section id="animate-area">
-        <div class="newfeaturesheader mx-16 ">
-        </div>
-    </section>
-</div>
-
-</template>
-
-<script>
-
-export default {
-    name: 'AnimatedSlide'
-}
-
-</script>
